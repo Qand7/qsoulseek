6485cc7ea0288594df0ec2603080e82eb5eff9e7
Qandy <145124536+Qand7@users.noreply.github.com>
2026-05-25 20:46:46 +0200

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -9,5 +9,8 @@ sudo rm /usr/bin/soulseek
 sudo chmod -x /usr/share/applications/soulseek.desktop
 sudo rm /usr/share/applications/soulseek.desktop
 
+sudo chmod -x /usr/share/soulseek/soulseekqt.AppImage
+sudo rm -rf /usr/share/soulseek
+
 # Keep the icon file tbh, if you really want it gone, uncomment the following line before running this file
 # sudo rm /usr/share/icons/hicolor/scalable/apps/soulseek.svg
